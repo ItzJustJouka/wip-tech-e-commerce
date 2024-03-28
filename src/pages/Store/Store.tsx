3d53574f96de5315978dc63f2338eb4bbc7d56b9
GiorgioDiBenn <giorgio.dibennardo@stevejobs.academy>
2024-03-28 15:41:35 +0100

    feat: Added Navbar

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -5,14 +5,15 @@ import Card from '../../components/Card/Card';
 import Header from '../../components/Header/Header';
 import CartItems from '../../types/CartItems';
 import CartProducts from '../../components/CartProducts/CartProducts';
-import "./Store.css"
-
+import "./Store.css";
+import Navbar from '../../components/Navbar/Navbar';
 const Store = () => {
     const { products } = useCrud();
     const [cartItems, setCartItems] = useState<CartItems[]>([]);
 
     return (
         <>
+            <Navbar></Navbar>
             <Header cartItems={cartItems} />
             <div className="cards__container">
                 {products ? (
