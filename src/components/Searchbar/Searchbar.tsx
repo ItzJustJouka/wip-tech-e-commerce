ef98c75ccaff0d8734929d3b8c749816fe7070d0
ItzJustJouka <matteopuglisif@gmail.com>
2024-03-28 12:18:19 +0100

    Adjusted CSS

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -2,13 +2,11 @@ import "./Searchbar.css"
 
 const Searchbar = () => {
   return (
-    <div>
-
-      <input type="search" placeholder="search"></input>
-      <button type="submit" className="btn btn-outline-primary">Search</button>
-      
+    <div className="search-bar__container">
+      <input type="search" placeholder="Search" className="search-bar" />
+      <button type="submit" className="btn btn-outline-primary search-button">Search</button>
     </div>
   );
 }
 
-export default Searchbar;
+export default Searchbar
