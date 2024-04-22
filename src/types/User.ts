a757d8dd68b02e4f8b0644d11fa7c4a0d44b8bba
Matteo Francesco Puglisi <matteopuglisif@gmail.com>
2024-04-22 13:16:25 +0200

    fix: Comments Removed

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,7 +1,6 @@
 type User = {
-    name: string;
+    fullName: string;
     email: string;
     password: string;
-    [key: string]: string; // This line is the index signature
 }
 export default User
