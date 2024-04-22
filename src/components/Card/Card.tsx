96066373996a8accb9ea4dd61b74bd49e0dea693
Matteo Francesco Puglisi <matteopuglisif@gmail.com>
2024-04-22 13:21:19 +0200

    fix: Card Structure

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import "./Card.css";
 import Product from "../../types/Product";
 import CartItems from "../../types/CartItems";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 interface CardProps {
     item: Product,
@@ -15,11 +17,23 @@ const Card = ({ item, setCartItems }: CardProps) => {
     }
 
     return (
-        <div className="card product-card" style={{ backgroundImage: "linear-gradient(to bottom right, #FFFFFF 0%, #DFDFEF 100%)" }}>
-            <img src={item.image} className="card-img-top" alt={"card"} />
+        <div className="card product-card product" style={{ backgroundImage: "linear-gradient(to bottom right, #FFFFFF 0%, #DFDFEF 100%)" }}>
+            <a href={`/product/${item.id}`}>
+                <div className="card-img-container">
+                    <img src={item.image} className="card-img-top" alt={item.title} />
+                </div>
+            </a>
             <div className="card-body">
-                <h5 className="card-title truncate">{item.title}</h5>
-                <p className="card-text truncate">{item.description}</p>
+
+                <h5 className="card-title truncate" id="product-name" >
+                    <a href={`/product/${item.id}`}>{item.title}</a>
+                </h5>
+
+                <p className="card-text" id="product-category">{item.category.split("").map((c, index) => index === 0 ? c.toUpperCase() : c).join("")}</p>
+                <div className="rating-container">
+                    <p className="card-text" id="product-rate__rating">{Array.from({ length: 5 }).map((_, i) => (item.rating && item.rating.rate) && Math.floor(item.rating.rate) > i ? <FontAwesomeIcon key={i} icon={faStar} style={{ color: "#FFD43B" }} /> : <FontAwesomeIcon key={i} icon={faStar} />)}</p>
+                    <p className="card-text" id="product-rate__count">{item.rating && item.rating.count}</p>
+                </div>
                 <div className="prod-quantity d-flex justify-content-center my-2">
                     <div className="add-to-cart__btn-container d-flex pb-2">
                         <button className="btn btn-warning add-to-cart__btn" onClick={() => addToCart(item)}>Add to Cart</button>
