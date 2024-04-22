0e240616caea550c8486b9116f521ef3cdd3aff9
Matteo Francesco Puglisi <matteopuglisif@gmail.com>
2024-04-22 13:24:06 +0200

    feat: Added Checkout

--- src/components/Offcanvas/Offcanvas.tsx
+++ src/components/Offcanvas/Offcanvas.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CartItems from '../../types/CartItems'
 import CartProducts from '../CartProducts/CartProducts'
 import "./Offcanvas.css"
@@ -8,7 +8,16 @@ interface OffcanvasProps {
     setCartItems: Function
 }
 
-const Offcanvas: React.FC<OffcanvasProps> = ({cartItems, setCartItems}) => {
+const Offcanvas: React.FC<OffcanvasProps> = ({ cartItems, setCartItems }) => {
+    const [isCheckingOut, setIsCheckingOut] = useState<boolean>(false);
+    const handlePurchase = () => {
+        setIsCheckingOut(true);
+        setTimeout(() => {
+            alert("Thank you for your purchase!");
+            setCartItems([]);
+            setIsCheckingOut(false);
+        }, (Math.random() + 1) * 3000)
+    }
     return (
         <>
             <div className="offcanvas cart-offcanvas offcanvas-end" tabIndex={-1} id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
@@ -18,11 +27,18 @@ const Offcanvas: React.FC<OffcanvasProps> = ({cartItems, setCartItems}) => {
                 </div>
                 <div className="offcanvas-body">
                     {cartItems.length ? (
-                        <CartProducts cartItems={cartItems} setCartItems={setCartItems}/>
+                        <>
+                            <CartProducts cartItems={cartItems} setCartItems={setCartItems} />
+                            {!isCheckingOut ? (
+                                <button className="checkout-btn btn btn-warning" type="button" onClick={handlePurchase}>Checkout</button>
+                            ) : (
+                                <div className="loader"></div>
+                            )}
+                        </>
                     ) : (
                         <p>No items in your cart :|</p>
                     )}
-                    
+
                 </div>
             </div>
         </>
