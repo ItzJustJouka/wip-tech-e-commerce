655fb74857efab2f18481a85e8c932446ccd8a1e
Matteo Francesco Puglisi <matteopuglisif@gmail.com>
2024-04-22 13:23:43 +0200

    fix: Optimised CRUD

--- src/hooks/useCrud.tsx
+++ src/hooks/useCrud.tsx
@@ -40,8 +40,49 @@ const useCrud = () => {
     }
   }
 
+  const editProducts = async (product: Product) => {
+    try {
+      const response = await fetch(`http://localhost:3000/products/${product.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      });
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}
+      Error editing products: ${response.statusText}`);
+      }
+      const data = await response.json();
+      setProducts(products => products.map((prod) => prod.id === data.id ? data : prod));
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
 
-  return { products, setProducts, addProducts, getProducts };
+  const deleteProducts = async (product: Product) => {
+    try {
+      const response = await fetch(`http://localhost:3000/products/${product.id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      });
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}
+      Error editing products: ${response.statusText}`);
+      }
+      const data = await response.json();
+      setProducts(products => products.filter((prod) => prod.id !== data.id));
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  return { products, setProducts, addProducts, getProducts, editProducts, deleteProducts };
 }
 
 export default useCrud;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,19 +1,39 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Store from "../pages/Store/Store";
+import { useEffect, useState } from "react";
+import ProductPage from "../components/ProductPage/ProductPage";
+import CartItems from "../types/CartItems";
 
 const Router = () => {
+
+    const [cartItems, setCartItems] = useState<CartItems[]>([])
+
+    useEffect(() => {
+        const items = localStorage.getItem("cartItems");
+        setCartItems(items ? JSON.parse(items) : []);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    }, [cartItems]);
+
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <Store />,
+            element: <Store cartItems={cartItems} setCartItems={setCartItems} />,
         },
         {
             path: "/login",
             element: <Login />,
+        },
+        {
+            path: "/product/:id",
+            element: <ProductPage cartItems={cartItems} setCartItems={setCartItems} />,
         }
     ]
     )
     return <RouterProvider router={router} />
 }
+
 export default Router
